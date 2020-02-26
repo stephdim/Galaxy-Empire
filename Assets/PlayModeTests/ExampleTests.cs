4c0db89c1452824a1fe57effa3f7feedf6d52c37
stephdim <stephane.dimiceli@gmail.com>
2020-02-27 00:40:25 +0100

    Ajout des premiers TUs
    Ajout des classes d'exemple de TUs

---
+++ Assets/PlayModeTests/ExampleTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ExampleTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ExampleTestsSimplePasses()
+        {
+            Assert.Pass();
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ExampleTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
